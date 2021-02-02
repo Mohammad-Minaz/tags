e8d2d6b3f79857af53a676de8597ae9f69e59e37
Mohammad-Minaz <46309131+Mohammad-Minaz@users.noreply.github.com>
2021-02-02 15:56:14 +0530

    Update Tags.ps1

--- Tags.ps1
+++ Tags.ps1
@@ -1,3 +1,27 @@
+$connectionName = "AzureRunAsConnection"
+try
+{
+    # Get the connection "AzureRunAsConnection "
+    $servicePrincipalConnection=Get-AutomationConnection -Name $connectionName         
+ 
+    "Logging in to Azure..."
+    Connect-AzAccount `
+        -ServicePrincipal `
+        -TenantId $servicePrincipalConnection.TenantId `
+        -ApplicationId $servicePrincipalConnection.ApplicationId `
+        -CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint 
+}
+catch 
+{
+    if (!$servicePrincipalConnection)
+    {
+        $ErrorMessage = "Connection $connectionName not found."
+        throw $ErrorMessage
+    } else{
+        Write-Error -Message $_.Exception
+        throw $_.Exception
+    }
+}
 $VMs = Get-AzVm
 ForEach ($VM in $VMs){
 	if($VM | Where {$_.Tags."Name" -eq "$null" -Or $_.Tags."Owner" -eq "$null"  -Or $_.Tags."Team" -eq "$null" -Or $_.Tags."Timeline" -eq "$null" -Or $_.Tags."Project" -eq "$null" -Or $_.Tags."Role" -eq "$null"  -Or $_.Tags."Environment" -eq "$null"}){
